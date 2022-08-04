27a7ae6e8c946f02676f7955606542d0e4ef87b5
peijianan <1191181857@qq.com>
2022-08-04 14:33:50 +0800

    update

--- datamigration.py
+++ datamigration.py
@@ -1,16 +1,13 @@
 from sqlalchemy import create_engine,select,MetaData,Table,insert,delete
-import pymysql
-import psycopg2
 from sqlalchemy.exc import SQLAlchemyError
 import logging
+import threading 
+#import pymysql
+#import psycopg2
+metadata=MetaData()
 
 
-#日志设置
-logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                    level=logging.DEBUG,
-                    filename=r'C:\Users\aaa\Desktop\test.log',
-                    filemode='a')
-logging.getLogger('sqlalchemy.pool').setLevel(logging.INFO)
+
 
 
 #源数据库jbdc
@@ -21,6 +18,7 @@ engine2=create_engine('postgresql+psycopg2://postgres:13774529926@localhost:5432
 
 
 
+
 #数据表映射,源数据库表k：目标数据库表v
 My_Table={
           'tt':'tt_copy2',
@@ -28,32 +26,64 @@ My_Table={
 }
 
 
+def my_init():
+    #连接池初始化
+    for i in range(len(My_Table)):
+        c1=engine.connect()
+        c2=engine2.connect()
+        c1.execute('select 1')
+        c2.execute('select 1')
+        c1.close()
+        c2.close()
+    #日志设置
+    logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=logging.DEBUG,
+                    filename=r'C:\Users\aaa\Desktop\test.log',
+                    filemode='a')
+    logging.getLogger('sqlalchemy.pool').setLevel(logging.INFO)
+
 def my_migration(k,v):
-    metadata=MetaData()
+    con1=engine.connect()
+    con2=engine2.connect()   
     map1=Table(k,metadata,autoload=True,autoload_with=engine)
     map2=Table(v,metadata,autoload=True,autoload_with=engine2)
-    map2_delete=delete(map2)
+    map2_delete=delete(map2) #删除目标数据表数据
+    con2.execute(map2_delete)
     #map1_key=map1.columns.keys()
     #map2_key=map2.columns.keys()
-    engine2.execute(map2_delete)
     map1_select=select(map1)
-    results=engine.execute(map1_select).fetchall()
+    results=con1.execute(map1_select).fetchall()
     data = [dict(zip(result.keys(), result)) for result in results]
     
     try:
         ins=insert(map2)
-        engine2.execute(ins,data)
+        con2.execute(ins,data)
         logging.info('源数据库表'+k+'到目标数据库表'+v+'更新数据'+str(len(results))+'条！')
     except SQLAlchemyError as error:
         logging.error(error.orig.message, error.params)
+
+    con1.close()
+    con2.close()
 
 
 
 
 
 if __name__ == '__main__':
+
+
+    my_init()
+
+    thread_pool = [] #开启多线程
     for key,value in My_Table.items():
-        my_migration(key,value)
+        thread_pool.append(threading.Thread(target=my_migration,args=(key,value)))
+        #my_migration(key,value)
+    for th in thread_pool:
+        th.start()
+
+    for th in thread_pool:
+        th.join()
+
     logging.info('执行完毕！')
 
     
